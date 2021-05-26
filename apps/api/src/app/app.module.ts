bc8c4790caa7bdb194e4e30494352079e771381b
Doug Brown <dougb@finitemonkey.com>
2021-05-25 19:42:07 -0500

    Buggy stuff. Going to rebase.

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-
+import { Model } from 'mongoose';
 import { AppController } from './app.controller';
 import { GameController } from './game/game.controller';
 import { AppService } from './app.service';
 import { GameCreatorService } from './game/game-creator.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { environment } from '../environments/environment';
+import { Game, GameDocument } from './game/schemas/game.schema';
 
 
 @Module({
-  imports: [MongooseModule.forRoot(environment.mongoPrimary, {connectionName: 'electronic-detective',})],
+  imports: [MongooseModule.forRoot(environment.mongoPrimary, {connectionName: 'electronic-detective',}), Model<GameDocument>],
   controllers: [AppController, GameController],
   providers: [AppService, GameCreatorService],
 })
